cdc53e43ae2f34eed596f83c4a49298db8b4624a
Bijan Gurung <bijangurung99@gmail.com>
2023-06-18 08:16:38 +0545

    changes to storageSession

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -2,7 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import userSlice from "../reducers/userSlice";
 import logger from "redux-logger";
-import storageSession from 'redux-persist/lib/storage/session'
+import storageSession from 'redux-persist/lib/storage/session';
+import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
 
 
@@ -16,7 +17,7 @@ const reducer = combineReducers({
   user: userSlice
 });
 
-const persistedReducer = persistReducer(persistConfig, red)
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
